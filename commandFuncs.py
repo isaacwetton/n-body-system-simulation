314bb86ab9e2e9ff4d2238fe8c88aa60c2f5e8ae
Isaac Wetton <isaac@wetton.net>
2021-11-27 23:36:16 +0000

    Added commandFuncs.py to execute the given command (basic framework)

---
+++ commandFuncs.py
@@ -0,0 +1,14 @@
+def run_cmd(command):
+    if command == 'help':
+
+    elif command == 'add':
+
+    elif command == 'del':
+
+    elif command == 'plot':
+        
+
+# Console error message if script is run directly
+if __name__ == "__main__":
+    print("This python script is not intended to be run independently.\n")
+    input("Press the enter key to continue")
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Import modules
 from Particle import *
 import numpy as np
+import commandFuncs as cmd
 
 # Define list of commands
-COMMANDS = ["help", "exit", "add", "del", "plot"]
+COMMANDS = ["help", "add", "del", "plot"]
 # Print welcome message
 print("Welcome to this n-body gravity simulation by Isaac Wetton.\n\n"
       "The program initially has the Sun and Earth as the only objects.\n\n"
@@ -13,6 +14,7 @@ print("Welcome to this n-body gravity simulation by Isaac Wetton.\n\n"
 command = input("")
 
 # Check for valid command
-while command not in COMMANDS:
-    print("That is not a valid command. Type 'help' for a list of commands.")
-    command = input("")
+while command is not "exit":
+    while command not in COMMANDS:
+        print("That is not a valid command. Type 'help' for a list of commands.")
+        command = input("")
