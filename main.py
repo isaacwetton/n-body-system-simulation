a7d02e4a53ac47fcf9627f04098b6b4493b34bb5
Isaac Wetton <isaac@wetton.net>
2021-11-27 23:24:42 +0000

    Added welcome message and first command request

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 from Particle import *
 import numpy as np
 
+# Define list of commands
+COMMANDS = ["help", "exit", "add", "del", "plot"]
+# Print welcome message
+print("Welcome to this n-body gravity simulation by Isaac Wetton.\n\n"
+      "The program initially has the Sun and Earth as the only objects.\n\n"
+      "Type 'help' for a list of commands.\n")
+
+# Wait for user input
+command = input("")
+
+# Check for valid command
+while command not in COMMANDS:
+    print("That is not a valid command. Type 'help' for a list of commands.")
+    command = input("")
