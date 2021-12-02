a934e8cd42fec92d122b8f681cb83ef6267e71d0
Isaac Wetton <isaac@wetton.net>
2021-12-02 18:44:05 +0000

    Create systemEvolution.py
    Module includes function to evolve system and return final position and velocity

---
+++ systemEvolution.py
@@ -0,0 +1,10 @@
+from Particle import *
+
+def evolve_posvel(particle, deltaT, N, particle_dict):
+    time = 0
+    for i in range(N):
+        for object in particle_dict.items():
+            particle.updateGravitationalAcceleration(object)
+        particle.update(deltaT)
+        time += deltaT
+    return particle.position, particle.velocity
