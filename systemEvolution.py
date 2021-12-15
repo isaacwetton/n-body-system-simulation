ca6ab922fc04040327e02a1cfaa138f95b589a8b
Isaac Wetton <isaac@wetton.net>
2021-12-15 22:46:31 +0000

    Added framework for evolve_energy method

--- systemEvolution.py
+++ systemEvolution.py
@@ -1,5 +1,6 @@
 from Particle import *
 
+
 def evolve_posvel(particle, deltaT, N, m, particle_dict):
     time = 0
 
@@ -28,3 +29,30 @@ def evolve_posvel(particle, deltaT, N, m, particle_dict):
         x.append(particle.position[0])
         y.append(particle.position[1])
     return x, y
+
+def evolve_energy(particle, deltaT, N, m, particle_dict, n):
+    time = 0
+
+    # Define new dictionary without original particle
+
+    dict_wo_particle = {}
+    for x in particle_dict.keys():
+        if particle_dict[x] != particle:
+            dict_wo_particle[x] = particle_dict[x]
+
+    for i  in range(N):
+        accel = np.array([0, 0, 0], dtype=float)
+        for object in dict_wo_particle.values():
+            accel += particle.updateGravitationalAcceleration(object)
+        particle.acceleration = accel
+
+        # Update based on chosen method
+        if m == "Euler":
+            particle.update_euler(deltaT)
+        elif m == "EulerCromer":
+            particle.update_eulerCromer(deltaT)
+        time += deltaT
+
+        # Calculate total energy within system
+
+        energy = 0
