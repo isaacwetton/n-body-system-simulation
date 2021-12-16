b8f7f8efa86ffc8c670883a172615ec022fa7ef9
Isaac Wetton <isaac@wetton.net>
2021-12-16 16:43:46 +0000

    Added console error message if systemEvolution.py is run directly

--- systemEvolution.py
+++ systemEvolution.py
@@ -71,3 +71,8 @@ def evolve_momentum(particle, deltaT, m, particle_dict):
     # Calculate x, y, z momentum of particle
     momentum = particle.momentum()
     return particle, momentum
+
+# Console error message if script is run directly
+if __name__ == "__main__":
+    print("This python script is not intended to be run independently.\n")
+    input("Press the enter key to continue")
